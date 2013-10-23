d2407ba8ec40be1ececd36e19068e4b8439bb067
Cooper Mayne <coopermayne@gmail.com>
2013-10-23 13:20:45 -0400

    fixed sorting of sentences

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,11 +1,11 @@
 class StoriesController < ApplicationController
   def index
     #show the whole story (except last line)
-    @story = Story.all.first
+    @story = Story.map{|line_options| line_options.sort_by }
   end
 
   def next
     @story_id = Story.last.id
-    @nextline_options = Story.last.sentences
+    @nextline_options = Story.last.sentences.sort_by{|sentence| sentence.votes.inject(0) {|sum, vote| vote.vote==true ? sum + 1 : sum - 1}}.reverse
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,10 @@ class User < ActiveRecord::Base
   validates_presence_of :email
   validates_uniqueness_of :email
 
+  def logged_in? provider
+    self.authentications.select{|auths| auths.provider=="twitter"}.count > 0
+  end
+
   def self.authenticate(email, password)
     user = find_by_email(email)
     if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
