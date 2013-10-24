1a3a4bf81f0102985db00d5e50783efae309e942
Cooper Mayne <coopermayne@gmail.com>
2013-10-24 16:45:37 -0400

    small changes

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -3,6 +3,7 @@ class StoriesController < ApplicationController
   def index
     #show the whole story (except last line)
     @story = Story.all[0...-1].map{|line| line.sentences.sort_by{|sentence| sentence.votes.inject(0) {|sum, vote| vote.vote==true ? sum + 1 : sum - 1}}.reverse.first }
+    @author_list = @story.map{|line| line.user.name || line.user.authentications.first.username }.uniq
   end
 
   def next
