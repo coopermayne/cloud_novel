69f984b73c027a97d999aeb238798a9d8b474c4c
Cooper Mayne <coopermayne@gmail.com>
2013-10-24 21:30:56 -0400

    more fixes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,13 @@
 class VotesController < ApplicationController
 
   def create
+
+    unless current_user
+      flash[:notice] = "You can't vote without logging in sucker!"
+      redirect_to "/next"
+      return
+    end
+
     v = Vote.new
     v.vote = params[:vote]
     v.user = current_user
@@ -8,14 +15,15 @@ class VotesController < ApplicationController
     
     #OPTIMIZE: move this into the model fool
 
-    #if Vote.where(user_id: current_user.id,sentence_id: params[:sentence_id]).count > 0
-      #flash[:notice] = "You can't vote twice"
-      #redirect_to "/next"
-      #return
-    #end
+
+    if Vote.where(user_id: current_user.id,sentence_id: params[:sentence_id]).count > 0
+      flash[:notice] = "You can't vote twice"
+      redirect_to "/next"
+      return
+    end
 
     unless v.save
-      flash[:notice] = "You can't vote without logging in!"
+      flash[:notice] = "Problem saving! voted didn't count"
       redirect_to "/next"
       return
     end
@@ -23,9 +31,8 @@ class VotesController < ApplicationController
     #if the vote count is greater than 2
     # => create a new line in the story
     #   and append current
-
     #OPTIMIZE: move this to the model too fool
-    if Sentence.find(params[:sentence_id]).votes.count > 2
+    if Sentence.find(params[:sentence_id]).votes.inject(0) {|sum, vote| vote.vote==true ? sum + 1 : sum - 1} > 2
       redirect_to '/stories/create'
     else
       redirect_to "/next"
